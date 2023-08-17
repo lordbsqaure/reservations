88f8ca0c1e21ee0b6930688f9badbd343a0115a1
lordbsqaure <tse2ndi@gmail.com>
2023-08-17 10:16:36 +0100

    work

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,6 +1,8 @@
 import type { Prisma } from '@prisma/client'
 import { db } from 'api/src/lib/db'
 
+import { hashPassword } from '@redwoodjs/auth-dbauth-api'
+
 export default async () => {
   try {
     //
@@ -9,6 +11,7 @@ export default async () => {
     //
     // Update "const data = []" to match your data model and seeding needs
     //
+    const { salt } = '12345'
     const data: Prisma.UserExampleCreateArgs['data'][] = [
       // To try this example data with the UserExample model in schema.prisma,
       // uncomment the lines below and run 'yarn rw prisma migrate dev'
@@ -16,7 +19,7 @@ export default async () => {
       // { name: 'alice', email: 'alice@example.com' },
       // { name: 'mark', email: 'mark@example.com' },
       // { name: 'jackie', email: 'jackie@example.com' },
-      // { name: 'bob', email: 'bob@example.com' },
+      // { name: 'Admin', email: 'admin@gmail.com', salt: '' },
     ]
     console.log(
       "\nUsing the default './scripts/seed.{js,ts}' template\nEdit the file to add seed data\n"
@@ -24,38 +27,40 @@ export default async () => {
 
     // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
     // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
-    Promise.all(
-      //
-      // Change to match your data model and seeding needs
-      //
-      data.map(async (data: Prisma.UserExampleCreateArgs['data']) => {
-        const record = await db.userExample.create({ data })
-        console.log(record)
-      })
-    )
+    // Promise.all(
+    //   //
+    //   // Change to match your data model and seeding needs
+    //   //
+    //   data.map(async (data: Prisma.UserExampleCreateArgs['data']) => {
+    //     const record = await db.user.create({ data })
+    //     console.log(record)
+    //   })
+    // )
 
     // If using dbAuth and seeding users, you'll need to add a `hashedPassword`
     // and associated `salt` to their record. Here's how to create them using
     // the same algorithm that dbAuth uses internally:
     //
-    //   import { hashPassword } from '@redwoodjs/auth-dbauth-api'
+
     //
-    //   const users = [
-    //     { name: 'john', email: 'john@example.com', password: 'secret1' },
-    //     { name: 'jane', email: 'jane@example.com', password: 'secret2' }
-    //   ]
+    const users = [
+      { name: 'Admin', email: 'admin@gmail.com', password: 'secret1' },
+      //     { name: 'jane', email: 'jane@example.com', password: 'secret2' }
+    ]
     //
-    //   for (const user of users) {
-    //     const [hashedPassword, salt] = hashPassword(user.password)
-    //     await db.user.create({
-    //       data: {
-    //         name: user.name,
-    //         email: user.email,
-    //         hashedPassword,
-    //         salt
-    //       }
-    //     })
-    //   }
+    for (const user of users) {
+      const [hashedPassword, salt] = hashPassword(user.password)
+      await db.user.create({
+        data: {
+          name: user.name,
+          email: user.email,
+          role: 'ADMIN',
+          hashedPassword,
+
+          salt,
+        },
+      })
+    }
   } catch (error) {
     console.warn('Please define your seed data.')
     console.error(error)
